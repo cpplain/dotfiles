996e57d0a8ca3b4de47845071abc756599717eca
Christopher Plain <me@christopherplain.com>
2023-07-27 11:49:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotfiles
+
+My dotfiles are a major work in progress and are under constant revision as I discover ways to be a more productive developer. Feel free to use anything you may find useful.
+
+Because this repo is intended for my private use and is made public for informational purposes only, I won't be accepting issues or pull requests.
+
+Best wishes on your search for your ideal configs.
---
+++ tools/display_shell_colors.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+echo -e "\033[0mNO_COLOR"
+echo -e "\033[0;30mBLACK\t\t\033[1;30mDARK_GRAY"
+echo -e "\033[0;31mRED\t\t\033[1;31mLIGHT_RED"
+echo -e "\033[0;32mGREEN\t\t\033[1;32mLIGHT_GREEN"
+echo -e "\033[0;33mYELLOW\t\t\033[1;33mLIGHT_YELLOW"
+echo -e "\033[0;34mBLUE\t\t\033[1;34mLIGHT_BLUE"
+echo -e "\033[0;35mPURPLE\t\t\033[1;35mLIGHT_PURPLE"
+echo -e "\033[0;36mCYAN\t\t\033[1;36mLIGHT_CYAN"
+echo -e "\033[0;37mLIGHT_GRAY\t\033[1;37mWHITE"
