fe65cdffd8c3c129721f0422080327ad18e56002
Christopher Plain <me@christopherplain.com>
2023-07-27 12:07:17 -0700

    Use default Neovim config in VS Code

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,8 @@
+-- use default Neovim config if running in VS Code
+if vim.g.vscode then
+    print("Neovim running in VS Code. Using default config.")
+    return
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
