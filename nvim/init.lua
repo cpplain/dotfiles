bbc6b85346342cbca0273bbf288280a3a7fa35e4
Christopher Plain <me@christopherplain.com>
2023-08-28 19:01:36 -0700

    Remove LazyVim

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,5 +4,38 @@ if vim.g.vscode then
     return
 end
 
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- Set <space> as the leader key
+-- See `:help mapleader`
+-- NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Install package manager
+-- https://github.com/folke/lazy.nvim
+-- See `:help lazy.nvim.txt`
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Install plugins
+require("lazy").setup({
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("catppuccin")
+        end,
+    },
+}, {})
