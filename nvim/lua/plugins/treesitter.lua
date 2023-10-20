8824cdbdba7bd7b33b135f5495660d89af3e36fa
Christopher Plain <me@christopherplain.com>
2023-10-19 21:53:05 -0700

    Update treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
+-- See `:help nvim-treesitter`
 local M = { "nvim-treesitter/nvim-treesitter" }
 
 M.build = ":TSUpdate"
+
+-- See `:help nvim-treesitter-textobjects`
 M.dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" }
 
 function M.config()
@@ -15,8 +18,51 @@ function M.config()
             "vim",
             "vimdoc",
         },
-        highlight = {
-            enable = true,
+
+        -- See `:help nvim-treesitter-modules`
+        highlight = { enable = true },
+
+        -- See `:help nvim-treesitter-textobjects-modules`
+        textobjects = {
+            move = {
+                enable = true,
+                set_jumps = true,
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
+                },
+            },
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                },
+            },
+            swap = {
+                enable = true,
+                swap_next = {
+                    ["<leader>a"] = "@parameter.inner",
+                },
+                swap_previous = {
+                    ["<leader>A"] = "@parameter.inner",
+                },
+            },
         },
     })
 end
