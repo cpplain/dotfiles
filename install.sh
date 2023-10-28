c9fd424283e7c55ceb82e6390ba10115050eaab6
Christopher Plain <me@christopherplain.com>
2023-10-27 22:19:20 -0700

    Update dir structure to match system

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env zsh
+
+directories=(
+    .config
+    Library/Application\ Support/Code/User
+    Library/Application\ Support/iTerm2/DynamicProfiles
+)
+for directory in $directories; do
+    if [ ! -d ~/$directory ]; then
+        echo "Creating ~/$directory"
+        mkdir -p ~/$directory
+    fi
+done
+
+parent=$(dirname $(pwd))
+
+repo=$parent/dotfiles/home
+dotfiles=(
+    .config/git
+    .config/nvim
+    Library/Application\ Support/Code/User/settings.json
+    Library/Application\ Support/iTerm2/DynamicProfiles/profiles.json
+    .zshrc
+)
+for file in $dotfiles; do
+    echo "Creating symlink ~/$file"
+    if [ -e ~/$file ]; then
+        rm ~/$file
+    fi
+    ln -s $repo/$file ~/$file
+done
+
+repo=$parent/dotfiles-private/home
+dotfiles=(
+    .config/git-private
+)
+for file in $dotfiles; do
+    echo "Creating symlink ~/$file"
+    if [ -e ~/$file ]; then
+        rm ~/$file
+    fi
+    ln -s $repo/$file ~/$file
+done
