1110b4bf72f887611d3a541254f4ca6b7f2d1380
Christopher Plain <me@christopherplain.com>
2023-11-03 16:15:23 -0700

    Add font size and backround blur

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -13,6 +13,8 @@ end
 config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.font = wezterm.font("SF Mono")
+config.font_size = 12
+config.macos_window_background_blur = 100
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
 
