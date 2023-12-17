76f9e52e3270d6085e073ceef0b5d4e4c586293d
Christopher Plain <me@christopherplain.com>
2023-12-16 17:55:37 -0800

    Remove iTerm2, yabai, and skhd

--- install.zsh
+++ install.zsh
@@ -32,16 +32,12 @@ cd ~
 
 mkdir -p .config
 mkdir -p Library/Application\ Support/Code/User
-mkdir -p Library/Application\ Support/iTerm2/DynamicProfiles
 
 ln -sf $REPO_PUB/.config/git .config/
 ln -sf $REPO_PRV/.config/git/private .config/git/
 ln -sf $REPO_PUB/.config/fish .config/
 ln -sf $REPO_PUB/.config/nvim .config/
-ln -sf $REPO_PUB/.config/skhd .config/
 ln -sf $REPO_PUB/.config/starship.toml .config/
 ln -sf $REPO_PUB/.config/wezterm .config/
-ln -sf $REPO_PUB/.config/yabai .config/
 ln -sf $REPO_PUB/.zshrc ./
 ln -sf $REPO_PUB/Library/Application\ Support/Code/User/settings.json Library/Application\ Support/Code/User/
-ln -sf $REPO_PUB/Library/Application\ Support/iTerm2/DynamicProfiles/profiles.json Library/Application\ Support/iTerm2/DynamicProfiles/
