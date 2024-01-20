87e753d9b3feb8279dda6eabb7b56a2e80832a23
Christopher Plain <me@christopherplain.com>
2024-01-19 17:09:49 -0800

    Initial commit of minimal nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,27 @@
+vim.opt.colorcolumn = "80"
+vim.opt.conceallevel = 0
+vim.opt.termguicolors = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+})
