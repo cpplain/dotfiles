1686a3abf5e7d4fb65ce34920f100e8aa2ddac59
Christopher Plain <me@christopherplain.com>
2024-01-31 20:21:50 -0800

    Add commands

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 require("options")
 require("keymaps")
+require("commands")
 
 -- Plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
---
+++ .config/nvim/lua/commands.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
