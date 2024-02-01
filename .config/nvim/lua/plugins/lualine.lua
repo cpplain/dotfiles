24bdfc619884b1219101affa579fe3459f84faf9
Christopher Plain <me@christopherplain.com>
2024-01-31 21:38:38 -0800

    Add lualine plugin

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+-- Statusline
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			icons_enabled = false,
+			theme = "catppuccin",
+			component_separators = "",
+			section_separators = "",
+		},
+	},
+}
