04c2599faa2841f1a061c2862f55597b8d1a3379
Christopher Plain <me@christopherplain.com>
2024-02-04 20:18:15 -0800

    Move catppuccin setup to config function

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,15 +24,16 @@ require("lazy").setup({
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-		opts = {
-			integrations = {
-				lsp_trouble = true,
-				noice = true,
-				notify = true,
-				treesitter_context = true,
-			},
-		},
-		init = function()
+		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					lsp_trouble = true,
+					noice = true,
+					notify = true,
+					treesitter_context = true,
+				},
+			})
+
 			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
