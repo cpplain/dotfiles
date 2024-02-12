46665062d3551499f7c8cf5ac6d2d8ee44475f71
Christopher Plain <me@christopherplain.com>
2024-02-11 22:15:38 -0800

    Remove cmp super tab and simplify mappings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -331,12 +331,12 @@ require("lazy").setup({
 		},
 		config = function()
 			local cmp = require("cmp")
-			local luasnip = require("luasnip")
+			local select = { behavior = cmp.SelectBehavior.Select }
 
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						luasnip.lsp_expand(args.body)
+						require("luasnip").lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -344,25 +344,10 @@ require("lazy").setup({
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<CR>"] = cmp.mapping.confirm({ select = false }),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
+					["<C-n>"] = cmp.mapping.select_next_item(select),
+					["<C-p>"] = cmp.mapping.select_prev_item(select),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
 				}),
 				sources = cmp.config.sources({
 					{ name = "buffer" },
@@ -392,6 +377,9 @@ require("lazy").setup({
 			})
 
 			require("luasnip.loaders.from_vscode").lazy_load()
+
+			vim.keymap.set({ "i", "s" }, "<C-h>", "<Cmd>lua require('luasnip').jump(-1)<CR>")
+			vim.keymap.set({ "i", "s" }, "<C-l>", "<Cmd>lua require('luasnip').jump(1)<CR>")
 
 			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
