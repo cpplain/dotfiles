2760366ddf6581e252aff118066b2be75387af26
Christopher Plain <me@christopherplain.com>
2024-02-13 19:18:57 -0800

    Minor formatting mod

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -329,9 +329,13 @@ require("lazy").setup({
 		},
 		config = function()
 			require("noice").setup({
-				cmdline = { view = "cmdline" },
+				cmdline = {
+					view = "cmdline",
+				},
 				lsp = {
-					progress = { enabled = false },
+					progress = {
+						enabled = false,
+					},
 					override = {
 						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 						["vim.lsp.util.stylize_markdown"] = true,
