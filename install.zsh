f648fa8eaf9e9de29f0ac5b6191e3fa9eede3c52
Christopher Plain <christopherplain@Christophers-MacBook-Pro.local>
2024-02-19 14:03:13 -0800

    Organize configs by app

--- install.zsh
+++ install.zsh
@@ -1,44 +1,38 @@
-#!/bin/zsh
+#!/bin/bash
 
 if test $# = 0 || ! (test $1 = "personal" || test $1 = "work"); then
-    echo "Usage: install.sh 'personal' | 'work'"
-    exit 1
+	echo "Usage: install.sh <personal> | <work>"
+	exit 1
 fi
 
-if ! which brew > /dev/null; then
-  echo "Installing Homebrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! which brew >/dev/null; then
+	echo "Installing Homebrew"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  if test $(uname -m) = "arm64"; then
-      eval "$(/opt/homebrew/bin/brew shellenv)"
-  fi
+	if test $(uname -m) = "arm64"; then
+		eval "$(/opt/homebrew/bin/brew shellenv)"
+	fi
 
-  if test $(uname -m) = "x86_64"; then
-      eval "$(/usr/local/brew shellenv)"
-  fi
+	if test $(uname -m) = "x86_64"; then
+		eval "$(/usr/local/brew shellenv)"
+	fi
 fi
 
 echo "Installing packages"
 
-ln -sf Brewfile-$1 Brewfile
-brew bundle
+brew bundle --file brew-$1/.Brewfile --no-lock
 
 echo "Creating symlinks"
 
-REPO_PUB=$(pwd)
-REPO_PRV=$REPO_PUB-private
+mkdir -p ~/.config
+mkdir -p ~/Library/Application\ Support/Code/User
 
-cd ~
-
-mkdir -p .config
-mkdir -p Library/Application\ Support/Code/User
-
-ln -sf $REPO_PUB/.config/git .config/
-ln -sf $REPO_PRV/.config/git/private .config/git/
-ln -sf $REPO_PUB/.config/fish .config/
-ln -sf $REPO_PUB/.config/lazyvim .config/
-ln -sf $REPO_PUB/.config/nvim .config/
-ln -sf $REPO_PUB/.config/starship.toml .config/
-ln -sf $REPO_PUB/.config/wezterm .config/
-ln -sf $REPO_PUB/.zshrc ./
-ln -sf $REPO_PUB/Library/Application\ Support/Code/User/settings.json Library/Application\ Support/Code/User/
+ln -sF $(pwd)/brew-$1/.Brewfile ~/
+ln -sF $(pwd)/fish/.config/fish ~/.config
+ln -sF $(pwd)/git/.config/git ~/.config
+ln -sF $(pwd)/lazyvim/.config/lazyvim ~/.config
+ln -sF $(pwd)/nvim/.config/nvim ~/.config
+ln -sF $(pwd)/starship/.config/starship.toml ~/.config
+ln -sF $(pwd)/vscode/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User
+ln -sF $(pwd)/wezterm/.config/wezterm ~/.config
+ln -sF $(pwd)/zsh/.zshrc ~/
