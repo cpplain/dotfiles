7ed4f91b0ae709cb957e091a6d208544f1220d5f
Christopher Plain <me@christopherplain.com>
2024-02-24 19:57:11 -0800

    Enable wrap for markdown files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -81,6 +81,14 @@ vim.keymap.set("n", "<Leader>w", "<Cmd>set wrap!<CR>")
 --[[ Commands ]]
 --
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	group = vim.api.nvim_create_augroup("EnableWrap", { clear = true }),
+	callback = function()
+		vim.opt.wrap = true
+	end,
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
 	callback = function()
