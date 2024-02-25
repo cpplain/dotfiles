5cab4b9d5eccbd01bacff650fd8e78213ff4a850
Christopher Plain <me@christopherplain.com>
2024-02-24 17:46:24 -0800

    Add spell completion source

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -311,6 +311,7 @@ require("lazy").setup({
 			"saadparwaiz1/cmp_luasnip",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
+			"f3fora/cmp-spell",
 			-- Snippets
 			"L3MON4D3/LuaSnip",
 		},
@@ -334,6 +335,7 @@ require("lazy").setup({
 					{ name = "luasnip" },
 					{ name = "nvim_lsp" },
 					{ name = "path" },
+					{ name = "spell" },
 				}, {
 					{ name = "buffer" },
 				}),
