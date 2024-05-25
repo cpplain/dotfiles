055b7d0a7bc8bce4c570fc061eb9b888ee32a649
Christopher Plain <me@christopherplain.com>
2024-05-25 16:33:56 -0700

    Add lsp borders

--- home/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -14,15 +14,22 @@ return {
 
 		require("neodev").setup()
 
+		local border = "rounded"
+		local handlers = {
+			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+		}
+
 		local lspconfig = require("lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		for name, opts in pairs(servers) do
 			opts.capabilities = capabilities
+			opts.handlers = handlers
 
 			lspconfig[name].setup(opts)
 		end
 
-		require("lspconfig.ui.windows").default_options.border = "rounded"
+		require("lspconfig.ui.windows").default_options.border = border
 	end,
 }
