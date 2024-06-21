f2387dcbea6e2a5887d530f0aa4754d7c8c456e4
Christopher Plain <me@christopherplain.com>
2024-06-21 15:07:10 -0700

    Minor updates to wezterm config

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -2,7 +2,9 @@ local wezterm = require("wezterm")
 local mux = wezterm.mux
 local config = {}
 
---[[ Default Config ]]
+--
+-- Default Config
+--
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
@@ -10,12 +12,13 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
---[[ User Config ]]
+--
+-- User Config
+--
 
 -- Change default shell
-if wezterm.target_triple == "aarch64-apple-darwin" then
-	config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
-elseif wezterm.target_triple == "x86_64-apple-darwin" then
+config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+if wezterm.target_triple == "x86_64-apple-darwin" then
 	config.default_prog = { "/usr/local/bin/fish", "-l" }
 end
 
@@ -23,9 +26,7 @@ config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.font = wezterm.font("SF Mono")
 config.font_size = 12
--- config.macos_window_background_blur = 100
--- config.window_background_opacity = 0.9
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
 -- config.window_padding = {
 -- 	left = 20,
 -- 	right = 20,
