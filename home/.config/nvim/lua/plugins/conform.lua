c1affb1c6a7f1349671624d885d71d857a24e954
Christopher Plain <me@christopherplain.com>
2024-06-30 21:23:01 -0700

    Move formatters to top of file

--- home/.config/nvim/lua/plugins/conform.lua
+++ home/.config/nvim/lua/plugins/conform.lua
@@ -1,3 +1,20 @@
+local formatters = {
+	c = { "clang-format" },
+	css = { "prettierd" },
+	fish = { "fish_indent" },
+	go = { "goimports", "gofmt" },
+	html = { "prettierd" },
+	json = { "prettierd" },
+	javascript = { "prettierd" },
+	lua = { "stylua" },
+	markdown = { "prettierd" },
+	python = { "ruff_format" },
+	sh = { "shfmt" },
+	toml = { "taplo" },
+	typescript = { "prettierd" },
+	yaml = { "prettierd" },
+}
+
 return {
 	"stevearc/conform.nvim",
 	config = function()
@@ -5,22 +22,7 @@ return {
 			format_on_save = {
 				timeout_ms = 500,
 			},
-			formatters_by_ft = {
-				c = { "clang-format" },
-				css = { "prettierd" },
-				fish = { "fish_indent" },
-				go = { "goimports", "gofmt" },
-				html = { "prettierd" },
-				json = { "prettierd" },
-				javascript = { "prettierd" },
-				lua = { "stylua" },
-				markdown = { "prettierd" },
-				python = { "ruff_format" },
-				sh = { "shfmt" },
-				toml = { "taplo" },
-				typescript = { "prettierd" },
-				yaml = { "prettierd" },
-			},
+			formatters_by_ft = formatters,
 		})
 	end,
 }
