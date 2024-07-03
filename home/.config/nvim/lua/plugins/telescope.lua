0256bad752240f3cb1a0053953184c4782176a08
Christopher Plain <me@christopherplain.com>
2024-07-02 20:16:08 -0700

    Add fzf-native back to telescope.lua file

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -1,34 +1,40 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-fzf-native.nvim",
-	},
-	config = function()
-		local telescope = require("telescope")
-		telescope.setup({
-			defaults = {
-				layout_config = {
-					prompt_position = "top",
-				},
-				sorting_strategy = "ascending",
-				wrap_results = true,
-				file_ignore_patterns = {
-					".git",
-				},
-			},
-			pickers = {
-				find_files = {
-					hidden = true,
-				},
-				live_grep = {
-					additional_args = {
-						"--hidden",
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-fzf-native.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			telescope.setup({
+				defaults = {
+					layout_config = {
+						prompt_position = "top",
+					},
+					sorting_strategy = "ascending",
+					wrap_results = true,
+					file_ignore_patterns = {
+						".git",
 					},
 				},
-			},
-		})
-		telescope.load_extension("fzf")
-	end,
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+					live_grep = {
+						additional_args = {
+							"--hidden",
+						},
+					},
+				},
+			})
+			telescope.load_extension("fzf")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
 }
