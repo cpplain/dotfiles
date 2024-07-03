fad2a03dd6edb53776bfe4a86f6b9eabd0b97734
Christopher Plain <me@christopherplain.com>
2024-07-03 14:03:34 -0700

    Update to match latest installation recommendations

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,14 +1,8 @@
 -- Bootstrap plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,7 +10,10 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Set <Space> as <Leader>
 vim.g.maplocalleader = " " -- Set <Space> as <LocalLeader>
 
-require("lazy").setup("plugins", {
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+	},
 	install = { colorscheme = { "catppuccin" } },
 	ui = { border = "rounded" },
 })
