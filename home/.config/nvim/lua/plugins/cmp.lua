a87d0295ab408a4b70bf79a12d0bdb3232213aeb
Christopher Plain <me@christopherplain.com>
2024-07-03 20:04:11 -0700

    Refactor cmp spec

--- home/.config/nvim/lua/plugins/cmp.lua
+++ home/.config/nvim/lua/plugins/cmp.lua
@@ -1,92 +1,86 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			-- Sources
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-cmdline",
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-			-- Snippets
-			"L3MON4D3/LuaSnip",
-			"rafamadriz/friendly-snippets",
-			-- Autopairs
-			"windwp/nvim-autopairs",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-			cmp.setup({
-				-- completion = {
-				-- 	autocomplete = false,
-				-- },
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["Tab"] = cmp.mapping(function(fallback)
-						if luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["S-Tab"] = cmp.mapping(function(fallback)
-						if luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-				sources = cmp.config.sources({
-					-- group 1
-					{ name = "lazydev" },
-				}, {
-					-- group 2
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					--group 3
-					{ name = "buffer" },
-				}),
-			})
-
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					-- group 1
-					{ name = "path" },
-				}, {
-					-- group 2
-					{ name = "cmdline" },
-				}),
-			})
-		end,
+	"hrsh7th/nvim-cmp",
+	event = { "InsertEnter", "CmdlineEnter" },
+	dependencies = {
+		-- Sources
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-cmdline",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+		-- Snippets
+		{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+		"rafamadriz/friendly-snippets",
+		-- Autopairs
+		{ "windwp/nvim-autopairs", config = true },
 	},
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-		build = "make install_jsregexp",
-	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+
+		require("luasnip.loaders.from_vscode").lazy_load()
+
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+		cmp.setup({
+			-- completion = {
+			-- 	autocomplete = false,
+			-- },
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = {
+				["<C-n>"] = cmp.mapping(function()
+					if cmp.visible() then
+						cmp.select_next_item()
+					else
+						cmp.complete()
+					end
+				end, { "i", "c" }),
+				["<C-p>"] = cmp.mapping(function()
+					if cmp.visible() then
+						cmp.select_prev_item()
+					else
+						cmp.complete()
+					end
+				end, { "i", "c" }),
+				["<C-y>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
+				["<C-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
+			},
+			sources = cmp.config.sources({
+				-- group 1
+				{ name = "lazydev" },
+			}, {
+				-- group 2
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			}, {
+				--group 3
+				{ name = "buffer" },
+			}),
+		})
+
+		cmp.setup.cmdline({ "/", "?" }, {
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		cmp.setup.cmdline(":", {
+			sources = cmp.config.sources({
+				-- group 1
+				{ name = "path" },
+			}, {
+				-- group 2
+				{ name = "cmdline" },
+			}),
+		})
+	end,
 }
