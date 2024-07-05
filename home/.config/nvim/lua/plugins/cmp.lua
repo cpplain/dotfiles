6427f90ec54defab6eb9dc061ad199fbca5ab6fa
Christopher Plain <me@christopherplain.com>
2024-07-04 18:55:36 -0700

    Refactor cmp for better startup

--- home/.config/nvim/lua/plugins/cmp.lua
+++ home/.config/nvim/lua/plugins/cmp.lua
@@ -1,86 +1,97 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = { "InsertEnter", "CmdlineEnter" },
-	dependencies = {
-		-- Sources
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-cmdline",
-		"saadparwaiz1/cmp_luasnip",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-		-- Snippets
-		{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-		"rafamadriz/friendly-snippets",
-		-- Autopairs
-		{ "windwp/nvim-autopairs", config = true },
+	{
+		"hrsh7th/nvim-cmp",
+		event = { "InsertEnter", "CmdlineEnter" },
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				-- completion = {
+				-- 	autocomplete = false,
+				-- },
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = {
+					["<C-n>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							cmp.complete()
+						end
+					end, { "i", "c" }),
+					["<C-p>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							cmp.complete()
+						end
+					end, { "i", "c" }),
+					["<C-y>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
+					["<C-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
+				},
+				sources = cmp.config.sources({
+					-- group 1
+					{ name = "lazydev" },
+				}, {
+					-- group 2
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					--group 3
+					{ name = "buffer" },
+				}),
+			})
+
+			cmp.setup.cmdline({ "/", "?" }, {
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			cmp.setup.cmdline(":", {
+				sources = cmp.config.sources({
+					-- group 1
+					{ name = "path" },
+				}, {
+					-- group 2
+					{ name = "cmdline" },
+				}),
+			})
+		end,
 	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+	{
+		"L3MON4D3/LuaSnip",
+		build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		lazy = true,
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+	{
 
-		require("luasnip.loaders.from_vscode").lazy_load()
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup()
 
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-		cmp.setup({
-			-- completion = {
-			-- 	autocomplete = false,
-			-- },
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = {
-				["<C-n>"] = cmp.mapping(function()
-					if cmp.visible() then
-						cmp.select_next_item()
-					else
-						cmp.complete()
-					end
-				end, { "i", "c" }),
-				["<C-p>"] = cmp.mapping(function()
-					if cmp.visible() then
-						cmp.select_prev_item()
-					else
-						cmp.complete()
-					end
-				end, { "i", "c" }),
-				["<C-y>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
-				["<C-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
-			},
-			sources = cmp.config.sources({
-				-- group 1
-				{ name = "lazydev" },
-			}, {
-				-- group 2
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-			}, {
-				--group 3
-				{ name = "buffer" },
-			}),
-		})
-
-		cmp.setup.cmdline({ "/", "?" }, {
-			sources = {
-				{ name = "buffer" },
-			},
-		})
-
-		cmp.setup.cmdline(":", {
-			sources = cmp.config.sources({
-				-- group 1
-				{ name = "path" },
-			}, {
-				-- group 2
-				{ name = "cmdline" },
-			}),
-		})
-	end,
+			local cmp = require("cmp")
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
 }
