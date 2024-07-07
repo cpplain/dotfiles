857f2f9030f24bf81671e7c4c404cd014cdfa3d7
Christopher Plain <me@christopherplain.com>
2024-07-06 18:27:40 -0700

    Update options

--- home/.config/nvim/after/plugin/mappings.lua
+++ home/.config/nvim/after/plugin/mappings.lua
@@ -18,11 +18,17 @@ vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
+-- Search
+vim.keymap.set("n", "<Leader>nh", "<Cmd>noh<CR>")
+
 -- Scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+--Spelling
+vim.keymap.set("n", "<Leader>sp", "<Cmd>set spell!<CR>")
 
 -- Wrapping lines
 vim.keymap.set("n", "<Leader>w", "<Cmd>set wrap!<CR>")
--- home/.config/nvim/after/plugin/options.lua
+++ home/.config/nvim/after/plugin/options.lua
@@ -1,28 +1,24 @@
--- Indentation and wrapping
-vim.opt.breakindent = true -- Wrapped line repeats indent
-vim.opt.expandtab = true -- Use spaces when tab is inserted
-vim.opt.linebreak = true -- Wrap lines are word boundary
-vim.opt.shiftwidth = 4 -- Number of spaces for autoindent
-vim.opt.tabstop = 4 -- Number of spaces tab uses
-vim.opt.wrap = false -- Long lines wrap to the next line
+vim.opt.breakindent = true
+vim.opt.expandtab = true
+vim.opt.linebreak = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.wrap = false
 
--- OS integration
-vim.opt.clipboard = "unnamedplus" -- Sync Neovim and OS clipboard
+vim.opt.clipboard = "unnamedplus"
 
--- Search
-vim.opt.hlsearch = false -- Highlight matches with last search pattern
-vim.opt.ignorecase = true -- Ignore case in search patterns
-vim.opt.smartcase = true -- Do not ignore case when pattern has uppercase
+vim.opt.completeopt = "menu,menuone,noselect"
 
--- Spelling
-vim.opt.spell = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+
 vim.opt.spelllang = "en_us"
 
--- UI
-vim.opt.completeopt = "menu,menuone,noselect" -- Options for Insert mode completion
-vim.opt.number = true -- Print the line numbers
-vim.opt.relativenumber = true -- Show relative line numbers
-vim.opt.signcolumn = "yes" -- Always display the sign column
-vim.opt.splitbelow = true -- Split new window below current
-vim.opt.splitright = true -- Split new window right of current
-vim.opt.termguicolors = true -- Enable 24-bit RGB color
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.termguicolors = true
