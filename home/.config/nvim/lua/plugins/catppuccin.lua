2791fc0665e52d89274ea7d37ef534f8be1f9194
Christopher Plain <me@christopherplain.com>
2024-07-08 10:29:17 -0700

    Minor updates to catppuccin

--- home/.config/nvim/lua/plugins/catppuccin.lua
+++ home/.config/nvim/lua/plugins/catppuccin.lua
@@ -4,9 +4,37 @@ return {
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
+			dim_inactive = { enabled = true },
+			default_integrations = false,
 			integrations = {
+				cmp = true,
+				gitsigns = true,
 				lsp_trouble = true,
-				treesitter_context = true,
+				markdown = true,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+						ok = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+						ok = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+				telescope = {
+					enabled = true,
+				},
+				treesitter = true,
 			},
 		})
 
