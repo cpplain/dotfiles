4e4fc52fa77d1697b0eda605ad8d03bfd3e36686
Christopher Plain <me@christopherplain.com>
2024-07-27 12:14:59 -0700

    Test load times with lazy.nvim in lua directory

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,25 +1,2 @@
--- Bootstrap plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--branch=stable", "--filter=blob:none", lazyrepo, lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Must remap leader before initializing lazy
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-require("lazy").setup("plugins", {
-	install = { colorscheme = { "catppuccin" } },
-	ui = { border = "rounded" },
-	change_detection = { notify = false },
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"netrwPlugin",
-				"tutor",
-			},
-		},
-	},
-})
+-- bootstrap plugin manager
+require("config.lazy")
---
+++ home/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,26 @@
+-- Bootstrap plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--branch=stable", "--filter=blob:none", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Must remap leader before initializing lazy
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("lazy").setup("plugins", {
+	rocks = { enabled = false },
+	install = { colorscheme = { "catppuccin" } },
+	ui = { border = "rounded" },
+	change_detection = { notify = false },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"netrwPlugin",
+				"tutor",
+			},
+		},
+	},
+})
