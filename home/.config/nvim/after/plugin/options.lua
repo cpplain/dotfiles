6e2009374c8490e7fe34c7cea6d0df76a819c224
Christopher Plain <me@christopherplain.com>
2024-07-27 19:01:41 -0700

    Disable providers

--- home/.config/nvim/after/plugin/options.lua
+++ home/.config/nvim/after/plugin/options.lua
@@ -1,3 +1,8 @@
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 vim.opt.breakindent = true
 vim.opt.expandtab = true
 vim.opt.linebreak = true
