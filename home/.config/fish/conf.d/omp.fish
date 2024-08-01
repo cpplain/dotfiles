d2bca6d2eb9b76aa6bf23efcb1d67bb20b9f1747
Christopher Plain <me@christopherplain.com>
2024-07-31 20:59:34 -0700

    Rename posh variables

--- home/.config/fish/conf.d/omp.fish
+++ home/.config/fish/conf.d/omp.fish
@@ -1,10 +1,10 @@
 oh-my-posh init fish --config ~/.config/omp.toml | source
 
-function set_poshcontext
-    set -e OMP_MODE
+function set_poshcontext --no-scope-shadowing
+    set -e OMP_BIND_MODE
     set -e OMP_GIT_BRANCH
 
-    set -gx OMP_MODE $fish_bind_mode
+    set -gx OMP_BIND_MODE $fish_bind_mode
 
     if git branch --show-current 2>/dev/null | read -l git_branch
         set -gx OMP_GIT_BRANCH $git_branch
