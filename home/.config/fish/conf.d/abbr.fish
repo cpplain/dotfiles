9e23ca7d9bd575f5e3e5dbff621d4fff0572629e
Christopher Plain <me@christopherplain.com>
2024-08-09 16:26:31 -0700

    More abbreviations

--- home/.config/fish/conf.d/abbr.fish
+++ home/.config/fish/conf.d/abbr.fish
@@ -1,3 +1,16 @@
 abbr cmbb cfgmanager --brew-bundle
 abbr cmcl cfgmanager --create-links
 abbr cmpl cfgmanager --prune-links
+
+abbr g git
+abbr gb git branch
+abbr gc git commit
+abbr gco git checkout
+abbr gf git fetch
+abbr gm git merge
+abbr gpll git pull
+abbr gpsh git push
+abbr gr git rebase
+abbr gs git status
+
+abbr n nvim
