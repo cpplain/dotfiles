76fb44788d5932ecb3ae8fdf7ce34e3971bff5d2
Christopher Plain <me@christopherplain.com>
2024-09-09 11:50:23 -0700

    Add git add abbr

--- home/.config/fish/conf.d/abbr.fish
+++ home/.config/fish/conf.d/abbr.fish
@@ -1,18 +1,19 @@
-abbr cmbb cfgmanager --brew-bundle
-abbr cmcl cfgmanager --create-links
-abbr cmpl cfgmanager --prune-links
+abbr cmbb "cfgmanager --brew-bundle"
+abbr cmcl "cfgmanager --create-links"
+abbr cmpl "cfgmanager --prune-links"
 
 abbr g git
-abbr gb git branch
-abbr gc git commit
-abbr gco git checkout
-abbr gd git diff
-abbr gf git fetch
-abbr gm git merge
-abbr gpull git pull
-abbr gpush git push
-abbr gr git rebase
-abbr gs git status
+abbr ga "git add"
+abbr gb "git branch"
+abbr gc "git commit"
+abbr gco "git checkout"
+abbr gd "git diff"
+abbr gf "git fetch"
+abbr gm "git merge"
+abbr gpull "git pull"
+abbr gpush "git push"
+abbr gr "git rebase"
+abbr gs "git status"
 
 abbr n nvim
-abbr n. nvim .
+abbr n. "nvim ."
