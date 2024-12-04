62c649c710f7344655bf043e95ff37d7c2456d4c
Christopher Plain <me@christopherplain.com>
2024-12-04 12:20:15 -0800

    Switching back to current treesitter release

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -17,24 +17,24 @@ local parsers = {
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	branch = "main",
 	lazy = false,
 	config = function()
-		require("nvim-treesitter").setup({
-			ensure_install = { parsers },
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = parsers,
+			auto_install = false,
 		})
 
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function(args)
-				local config = require("nvim-treesitter.config")
-				local ft = vim.bo[args.buf].filetype
-				local lang = vim.treesitter.language.get_lang(ft) or ft
-
-				if vim.list_contains(config.installed_parsers(), lang) then
-					vim.treesitter.start()
-					vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
-				end
-			end,
-		})
+		-- vim.api.nvim_create_autocmd("FileType", {
+		-- 	callback = function(args)
+		-- 		local config = require("nvim-treesitter.config")
+		-- 		local ft = vim.bo[args.buf].filetype
+		-- 		local lang = vim.treesitter.language.get_lang(ft) or ft
+		--
+		-- 		if vim.list_contains(config.installed_parsers(), lang) then
+		-- 			vim.treesitter.start()
+		-- 			vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+		-- 		end
+		-- 	end,
+		-- })
 	end,
 }
