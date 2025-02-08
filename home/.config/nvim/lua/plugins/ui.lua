13b61564f6660df81b887182815caf6edb070dac
Christopher Plain <me@christopherplain.com>
2025-02-08 12:12:11 -0800

    Disable floating cmdline

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -12,7 +12,11 @@ return {
 	{
 		"folke/noice.nvim",
 		opts = {
+			cmdline = {
+				view = "cmdline",
+			},
 			presets = {
+				command_palette = false,
 				lsp_doc_border = true,
 			},
 		},
