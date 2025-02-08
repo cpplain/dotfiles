8b04c564a6b99108a10a9e7409ba4af56ebca623
Christopher Plain <me@christopherplain.com>
2025-02-08 12:13:07 -0800

    Restore sane defaults to blink.cmp

--- home/.config/nvim/lua/plugins/coding.lua
+++ home/.config/nvim/lua/plugins/coding.lua
@@ -1,9 +1,13 @@
 return {
 	"saghen/blink.cmp",
 	opts = {
+		keymap = require("blink.cmp.config").keymap,
 		completion = {
 			menu = { border = "rounded" },
 			documentation = { window = { border = "rounded" } },
+		},
+		sources = {
+			cmdline = require("blink.cmp.config.sources").default.cmdline,
 		},
 		signature = { window = { border = "rounded" } },
 	},
