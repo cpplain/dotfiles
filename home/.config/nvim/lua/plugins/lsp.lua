bf34f652d2d17e5036d6399d46510b7d3b066dfe
Christopher Plain <me@christopherplain.com>
2025-02-10 18:02:37 -0800

    Disable inlay hints

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,14 @@
 return {
-	"williamboman/mason.nvim",
-	opts = {
-		ui = { border = "rounded" },
+	{
+		"nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = false },
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = { border = "rounded" },
+		},
 	},
 }
