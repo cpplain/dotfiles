069ad16c8b2b5ea7285de6596309c09592dfd49e
Christopher Plain <me@christopherplain.com>
2025-02-10 22:41:55 -0800

    Disable ghost text

--- home/.config/nvim/lua/plugins/coding.lua
+++ home/.config/nvim/lua/plugins/coding.lua
@@ -5,6 +5,9 @@ return {
 		completion = {
 			menu = { border = "rounded" },
 			documentation = { window = { border = "rounded" } },
+			ghost_text = {
+				enabled = false,
+			},
 		},
 		sources = {
 			cmdline = require("blink.cmp.config.sources").default.cmdline,
