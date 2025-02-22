7c5d89a075eeffaa81f079afb40728b43c03e588
Christopher Plain <me@christopherplain.com>
2025-02-21 20:09:23 -0800

    Disable blink.cmp menu auto show

--- home/.config/nvim/lua/plugins/coding.lua
+++ home/.config/nvim/lua/plugins/coding.lua
@@ -3,11 +3,12 @@ return {
 	opts = {
 		keymap = require("blink.cmp.config").keymap,
 		completion = {
-			menu = { border = "rounded" },
-			documentation = { window = { border = "rounded" } },
-			ghost_text = {
-				enabled = false,
+			menu = {
+				border = "rounded",
+				auto_show = false,
 			},
+			documentation = { window = { border = "rounded" } },
+			ghost_text = { enabled = false },
 		},
 		cmdline = require("blink.cmp.config.modes.cmdline").default,
 		signature = { window = { border = "rounded" } },
