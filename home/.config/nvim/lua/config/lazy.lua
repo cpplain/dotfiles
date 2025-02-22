d84fe258d275f6fa895008ddb85b92154e3c8ccf
Christopher Plain <me@christopherplain.com>
2025-02-22 12:46:01 -0800

    Add prettier extra

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -10,6 +10,7 @@ require("lazy").setup({
 		{ "LazyVim/LazyVim" },
 		{ import = "lazyvim.plugins" },
 		{ import = "lazyvim.plugins.extras.coding.blink" },
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.clangd" },
 		{ import = "lazyvim.plugins.extras.lang.git" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
