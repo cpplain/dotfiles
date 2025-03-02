830c0c51c03c26ed81cd55a437b8bb89c0b02d9d
Christopher Plain <me@christopherplain.com>
2025-03-01 20:31:10 -0800

    Modify default LazyVim options

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,3 +1,19 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+vim.opt.breakindent = true
+vim.opt.relativenumber = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.filetype.add({
+	extension = {
+		h = "c",
+	},
+	pattern = {
+		["B?b?rewfile.*"] = "ruby",
+		[".*/git/config.*"] = "gitconfig",
+	},
+})
