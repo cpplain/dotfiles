ec8a1cd85ecffc5b4422db2286208229038f650f
Christopher Plain <me@christopherplain.com>
2025-03-04 11:24:33 -0800

    Link to top-level files/directories

--- scripts/links.sh
+++ scripts/links.sh
@@ -5,30 +5,18 @@ source ~/.env || { echo "Unable to source .env" && exit 1; }
 pub_home=$DOTFILES_REPO_DIR/home
 prv_home=$DOTFILES_REPO_DIR/private/home
 
-find_dirs() {
-	find $1 -type d -mindepth 1 | sed "s|$1/||" | sort
-}
-
-find_files() {
-	find $1 -type f | sed "s|$1/||" | sort
-}
-
 create_links() {
-	echo "Creating directories"
-	{ find_dirs $pub_home && find_dirs $prv_home; } |
-		sort | uniq | xargs -I % bash -c 'mkdir -p ~/%; echo ~/%'
-
-	echo "Creating links"
-	find_files $pub_home | xargs -I % bash -c "ln -sf $pub_home/% ~/%; echo ~/%"
-	find_files $prv_home | xargs -I % bash -c "ln -sf $prv_home/% ~/%; echo ~/%"
+    echo "Creating links"
+    find $pub_home -depth 1 | sed "s|$pub_home/||" | sort | xargs -I % bash -c "ln -sF $pub_home/% ~/%; echo ~/%"
+    find $prv_home -type f | sed "s|$prv_home/||" | sort | xargs -I % bash -c "ln -sf $prv_home/% ~/%; echo ~/%"
 }
 
 prune_links() {
-	echo "Pruning broken links"
-	find -L ~ -path ~/Library -prune -o -lname $DOTFILES_REPO_DIR/\* -exec rm -f {} \; -print 2>/dev/null
+    echo "Pruning broken links"
+    find -L ~ -path ~/Library -prune -o -lname $DOTFILES_REPO_DIR/\* -exec rm -f {} \; -print 2>/dev/null
 }
 
 remove_links() {
-	echo "Removing all links"
-	find ~ -path ~/Library -prune -o -lname $DOTFILES_REPO_DIR/\* -exec rm -f {} \; -print 2>/dev/null
+    echo "Removing all links"
+    find ~ -path ~/Library -prune -o -lname $DOTFILES_REPO_DIR/\* -exec rm -f {} \; -print 2>/dev/null
 }
