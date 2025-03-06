4405606c9aab3f9a08bbb3b3b5c86c11e9c47ce3
Christopher Plain <me@christopherplain.com>
2025-03-05 20:29:02 -0800

    Enable markdown extras

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -8,6 +8,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = {
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.ruby" },
 		{ import = "lazyvim.plugins.extras.lang.rust" },
 		{ import = "plugins" },
