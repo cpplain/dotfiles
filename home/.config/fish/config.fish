1ba38bc40a9a4a533cb506e2bff2d5095396d14f
Christopher Plain <me@christopherplain.com>
2025-03-08 20:59:54 -0800

    Disable vi mode

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,14 +3,14 @@ set -gx VISUAL $EDITOR
 set -gx SUDO_EDITOR $EDITOR
 
 set -g fish_greeting # disable fish greeting
-set -g fish_key_bindings fish_vi_key_bindings
 set -g fish_prompt_pwd_dir_length 0
 
-set fish_vi_force_cursor 1
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
+#set -g fish_key_bindings fish_vi_key_bindings
+#set fish_vi_force_cursor 1
+#set fish_cursor_default block
+#set fish_cursor_insert line
+#set fish_cursor_replace_one underscore
+#set fish_cursor_visual block
 
 for line in (cat ~/.env | grep -v "^#")
     set item (string split -m 1 "=" $line)
