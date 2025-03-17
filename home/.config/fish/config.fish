76298773cdf609f8e99511f1dac10207f12435fb
Christopher Plain <me@christopherplain.com>
2025-03-17 12:55:05 -0700

    Enable vi mode

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -4,13 +4,13 @@ set -gx SUDO_EDITOR $EDITOR
 
 set -g fish_greeting # disable fish greeting
 set -g fish_prompt_pwd_dir_length 0
+set -g fish_key_bindings fish_vi_key_bindings
 
-#set -g fish_key_bindings fish_vi_key_bindings
-#set fish_vi_force_cursor 1
-#set fish_cursor_default block
-#set fish_cursor_insert line
-#set fish_cursor_replace_one underscore
-#set fish_cursor_visual block
+set fish_vi_force_cursor 1
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
 
 for line in (cat ~/.env | grep -v "^#")
     set item (string split -m 1 "=" $line)
