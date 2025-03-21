4684a7f669d9caa48bf140af37f0aefdf27a0b6c
Christopher Plain <me@christopherplain.com>
2025-03-21 13:56:12 -0700

    Remove git extra

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,11 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	opts = {
 		ensure_installed = {
+			"git_config",
+			"git_rebase",
+			"gitattributes",
+			"gitcommit",
+			"gitignore",
 			"make",
 			"puppet",
 		},
