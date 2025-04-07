d95ed002106f5a8d88f94b7b435e6b5097da1db2
Christopher Plain <me@christopherplain.com>
2025-04-07 16:06:47 -0700

    .

--- home/.config/nvim/lua/plugins/spec.lua
+++ home/.config/nvim/lua/plugins/spec.lua
@@ -232,22 +232,7 @@ return {
 	},
 	{
 		"echasnovski/mini.pairs",
-		event = "VeryLazy",
-		opts = {
-			modes = { insert = true, command = true, terminal = false },
-			-- skip autopair when next character is one of these
-			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-			-- skip autopair when the cursor is inside these treesitter nodes
-			skip_ts = { "string" },
-			-- skip autopair when next character is closing pair
-			-- and there are more closing pairs than opening pairs
-			skip_unbalanced = true,
-			-- better deal with markdown code blocks
-			markdown = true,
-		},
-		config = function(_, opts)
-			LazyVim.mini.pairs(opts)
-		end,
+		enabled = false,
 	},
 	{
 		"folke/noice.nvim",
@@ -306,6 +291,11 @@ return {
 			end
 			require("noice").setup(opts)
 		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "VeryLazy",
+		opts = {},
 	},
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
