a0987368550999f093842a4ceebb3646cb79434b
Christopher Plain <me@christopherplain.com>
2025-04-08 14:33:47 -0700

    .

--- home/.config/nvim/lua/plugins/spec.lua
+++ home/.config/nvim/lua/plugins/spec.lua
@@ -484,6 +484,68 @@ local trouble = {
 	},
 }
 
+local which_key = {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	keys = {
+		{
+			"<Leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Keymaps (which-key)",
+		},
+		{
+			"<C-w><Space>",
+			function()
+				require("which-key").show({ keys = "<C-w>", loop = true })
+			end,
+			desc = "Window Hydra Mode (which-key)",
+		},
+	},
+	opts = {
+		preset = "helix",
+		spec = {
+			{
+				mode = { "n", "v" },
+				{ "<Leader><Tab>", group = "tabs" },
+				{ "<Leader>c", group = "code" },
+				{ "<Leader>d", group = "debug" },
+				{ "<Leader>dp", group = "profiler" },
+				{ "<Leader>f", group = "file/find" },
+				{ "<Leader>g", group = "git" },
+				{ "<Leader>gh", group = "hunks" },
+				{ "<Leader>q", group = "quit/session" },
+				{ "<Leader>s", group = "search" },
+				{ "<Leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+				{ "<Leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+				{ "[", group = "prev" },
+				{ "]", group = "next" },
+				{ "g", group = "goto" },
+				{ "gs", group = "surround" },
+				{ "z", group = "fold" },
+				{
+					"<Leader>b",
+					group = "buffer",
+					expand = function()
+						return require("which-key.extras").expand.buf()
+					end,
+				},
+				{
+					"<Leader>w",
+					group = "windows",
+					proxy = "<C-w>",
+					expand = function()
+						return require("which-key.extras").expand.win()
+					end,
+				},
+				-- better descriptions
+				{ "gx", desc = "Open with system app" },
+			},
+		},
+	},
+}
+
 local disabled = {
 	{
 		"akinsho/bufferline.nvim",
@@ -540,5 +602,6 @@ return {
 	render_markdown,
 	snacks,
 	trouble,
+	which_key,
 	disabled,
 }
