143c0baa256545eaaf7c460e3355f9ada46c5d6e
Christopher Plain <me@christopherplain.com>
2025-04-09 14:39:19 -0700

    .

--- home/.config/nvim/lua/config/autocmds.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("SetTextIndent", { clear = true }),
-    pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-    end,
-})
