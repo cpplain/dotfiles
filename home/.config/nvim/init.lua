e496fe4a650e035a2960c364165e5b77a12f02d2
Christopher Plain <me@christopherplain.com>
2025-04-11 22:05:33 -0700

    .

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -6,14 +6,30 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.o.shell = "/opt/homebrew/bin/fish"
-
 vim.opt.breakindent = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = "menu,menuone,noselect"
 vim.opt.conceallevel = 0
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.linebreak = true
 vim.opt.list = false
+vim.opt.mouse = "a"
+vim.opt.number = true
 vim.opt.relativenumber = false
+vim.opt.scrolloff = 4
+vim.opt.shiftround = true
 vim.opt.shiftwidth = 4
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.spelllang = { "en" }
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.wrap = false
 
 local map = vim.keymap.set
 
