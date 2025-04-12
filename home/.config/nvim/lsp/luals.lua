691cee9d5e3f60ca58b20dbe59fe77743a405d58
Christopher Plain <me@christopherplain.com>
2025-04-11 17:52:14 -0700

    .

--- home/.config/nvim/lsp/luals.lua
+++ home/.config/nvim/lsp/luals.lua
@@ -16,22 +16,8 @@ return {
             workspace = {
                 checkThirdParty = false,
             },
-            codeLens = {
-                enable = true,
-            },
             completion = {
                 callSnippet = "Replace",
-            },
-            doc = {
-                privateName = { "^_" },
-            },
-            hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
             },
         },
     },
