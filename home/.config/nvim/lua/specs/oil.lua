a17dbf7389719c22a1a0a4e78d4ac3dca9886724
Christopher Plain <me@christopherplain.com>
2025-04-14 10:57:50 -0700

    .

--- home/.config/nvim/lua/specs/oil.lua
+++ home/.config/nvim/lua/specs/oil.lua
@@ -2,6 +2,10 @@ return {
     "stevearc/oil.nvim",
     event = "VeryLazy",
 
+    keys = {
+        { "<Leader>-", "<Cmd>Oil --float<CR>", desc = "Open oil file explorer" },
+    },
+
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {
