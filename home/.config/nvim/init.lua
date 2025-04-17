9f06197a3932a80ecbccb7d17586b234fac227ee
Christopher Plain <me@christopherplain.com>
2025-04-17 13:19:41 -0700

    .

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -56,7 +56,7 @@ map("n", "<Esc>", function()
 end, { expr = true, desc = "Escape and clear search highlight" })
 
 map("n", "<Leader>cd", function()
-    vim.api.nvim_put({ vim.trim(vim.fn.system("date -Iseconds")) }, "c", true, true)
+    vim.api.nvim_put({ vim.trim(vim.fn.system({ "date", "-Iseconds" })) }, "c", true, true)
 end, { desc = "Insert date and time (ISO 8601)" })
 
 map("n", "<Leader>l", "<Cmd>Lazy<CR>", { desc = "Open lazy" })
