2c01809389795ee3caad58b829a80987887dc189
Christopher Plain <me@christopherplain.com>
2025-04-18 11:00:43 -0700

    .

--- home/.config/nvim/lua/specs/gitsigns.lua
+++ home/.config/nvim/lua/specs/gitsigns.lua
@@ -2,4 +2,11 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts = {},
+
+    -- stylua: ignore
+    keys = {
+        { "<Leader>gn", function() require("gitsigns").nav_hunk("next") end, desc = "Goto next hunk"},
+        { "<Leader>gp", function() require("gitsigns").nav_hunk("prev") end, desc = "Goto prev hunk"},
+    }
+,
 }
