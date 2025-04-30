4e67856889663ce8e1a9dee6a05cddf035b3d15f
Christopher Plain <me@christopherplain.com>
2025-04-30 11:47:23 -0700

    .

--- home/.config/fish/functions/fish_prompt.fish
+++
@@ -1,7 +0,0 @@
-function fish_prompt --description 'Write out the prompt'
-    set -l last_pipestatus $pipestatus
-    set -l status_color (set_color $fish_color_status)
-    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$status_color" $last_pipestatus)
-
-    echo -n -s (path basename (prompt_pwd))" " (fish_git_prompt "(%s) ") $prompt_status" " "> "
-end
--- home/.config/fish/functions/fish_title.fish
+++
@@ -1,5 +0,0 @@
-function fish_title
-    set -q argv[1]; or set argv (status current-command)
-
-    echo (prompt_pwd) : $argv
-end
