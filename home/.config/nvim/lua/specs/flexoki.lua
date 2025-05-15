7fb57163d0c0e292a75035de3c21a3bde6517ac3
Christopher Plain <me@christopherplain.com>
2025-05-15 09:44:03 -0700

    .

--- home/.config/nvim/lua/specs/flexoki.lua
+++ home/.config/nvim/lua/specs/flexoki.lua
@@ -1,12 +1,14 @@
 return {
     "cpplain/flexoki.nvim",
-    -- dir = "~/git/flexoki.nvim",
+    dir = "~/git/flexoki.nvim",
     lazy = false,
     priority = 1000,
 
+    ---@type flexoki.Config
     opts = {
         plugins = {
             "gitsigns",
+            "nvim_treesitter_context",
             "which_key",
         },
     },
