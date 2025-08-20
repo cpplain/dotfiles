8f7d81a8bc6c356df00b02596789c77c2d182217
Christopher Plain <me@christopherplain.com>
2025-08-19 21:35:09 -0700

    Change add/change git gutter signs

--- home/.config/nvim/lua/specs/gitsigns.lua
+++ home/.config/nvim/lua/specs/gitsigns.lua
@@ -1,11 +1,21 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {},
 
     -- stylua: ignore
     keys = {
         { "<Leader>gn", function() require("gitsigns").nav_hunk("next") end, desc = "Goto next hunk"},
         { "<Leader>gp", function() require("gitsigns").nav_hunk("prev") end, desc = "Goto prev hunk"},
+    },
+
+    opts = {
+        signs = {
+            add = { text = "+" },
+            change = { text = "~" },
+        },
+        signs_staged = {
+            add = { text = "+" },
+            change = { text = "~" },
+        },
     },
 }
