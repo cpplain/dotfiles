23ed517b3d2817087690d81fcf53ef85ad2fd7fb
Christopher Plain <me@christopherplain.com>
2025-08-21 12:24:35 -0700

    Remove snacks lazy key mappings

--- home/.config/nvim/lua/specs/snacks.lua
+++ home/.config/nvim/lua/specs/snacks.lua
@@ -1,54 +1,6 @@
 return {
     "folke/snacks.nvim",
-    lazy = false,
     priority = 1000,
-
-    -- stylua: ignore
-    keys = {
-        -- TODO: Add root and cwd versions where applicable (e.g., `.files()`)
-        { "<Leader>e", function() Snacks.explorer() end, desc = "Open file explorer (snacks)" },
-        { "<Leader>n", function() Snacks.picker.notifications() end, desc = "Show notification history" },
-        -- find
-        { "<Leader>fb", function() Snacks.picker.buffers() end, desc = "Find open buffer" },
-        { "<Leader>ff", function() Snacks.picker.files() end, desc = "Find files" },
-        { "<Leader>fr", function() Snacks.picker.recent() end, desc = "Find recent file" },
-        -- git
-        { "<Leader>gd", function() Snacks.picker.git_diff() end, desc = "Git diff" },
-        { "<Leader>gl", function() Snacks.picker.git_log() end, desc = "Git log" },
-        { "<Leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git log line" },
-        { "<Leader>gs", function() Snacks.picker.git_status() end, desc = "Git status" },
-        { "<Leader>gS", function() Snacks.picker.git_stash() end, desc = "Git stash" },
-        -- grep
-        { "<Leader>sb", function() Snacks.picker.lines() end, desc = "Grep buffer lines" },
-        { "<Leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep open buffers" },
-        { "<Leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
-        { "<Leader>sp", function() Snacks.picker.lazy() end, desc = "Grep plugin spec" },
-        { "<Leader>sw", function() Snacks.picker.grep_word() end, desc = "Grep visual selection or word", mode = { "n", "x" } },
-        -- search
-        { "<Leader>sa", function() Snacks.picker.autocmds() end, desc = "Search autocmds" },
-        { "<Leader>sd", function() Snacks.picker.diagnostics() end, desc = "Search diagnostics" },
-        { "<Leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Search buffer diagnostics" },
-        { "<Leader>sh", function() Snacks.picker.help() end, desc = "Search help" },
-        { "<Leader>sH", function() Snacks.picker.highlights() end, desc = "Search highlights" },
-        { "<Leader>si", function() Snacks.picker.icons() end, desc = "Search icons" },
-        { "<Leader>sj", function() Snacks.picker.jumps() end, desc = "Search jumps" },
-        { "<Leader>sk", function() Snacks.picker.keymaps() end, desc = "Search keymaps" },
-        { "<Leader>sm", function() Snacks.picker.marks() end, desc = "Search marks" },
-        { "<Leader>sM", function() Snacks.picker.man() end, desc = "Search man pages" },
-        { "<Leader>sq", function() Snacks.picker.qflist() end, desc = "Search quickfix list" },
-        { "<Leader>sR", function() Snacks.picker.resume() end, desc = "Search resume" },
-        { "<Leader>su", function() Snacks.picker.undo() end, desc = "Search undo history" },
-        -- LSP
-        { "<Leader>ld", function() Snacks.picker.lsp_definitions() end, desc = "Goto definition" },
-        { "<Leader>lD", function() Snacks.picker.lsp_declarations() end, desc = "Goto declaration" },
-        { "<Leader>lI", function() Snacks.picker.lsp_implementations() end, desc = "Goto implementation" },
-        { "<Leader>lr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "Goto references" },
-        { "<Leader>ly", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto type definition" },
-        -- other
-        { "<Leader>bd", function() Snacks.bufdelete() end, desc = "Delete buffer" },
-        { "<Leader>bo", function() Snacks.bufdelete.other() end, desc = "Delete other buffers" },
-        { "<Leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename file" },
-    },
 
     ---@type snacks.Config
     opts = {
@@ -105,10 +57,50 @@ return {
     config = function(_, opts)
         require("snacks").setup(opts)
 
+        local map = vim.keymap.set
+
+        -- stylua: ignore start
+        map("n", "<Leader>e", function() Snacks.explorer() end, { desc = "Open file explorer (snacks)" })
+        map("n", "<Leader>n", function() Snacks.picker.notifications() end, { desc = "Show notification history" })
+        map("n", "<Leader>bd", function() Snacks.bufdelete() end, { desc = "Delete buffer" })
+        map("n", "<Leader>bo", function() Snacks.bufdelete.other() end, { desc = "Delete other buffers" })
+        map("n", "<Leader>fb", function() Snacks.picker.buffers() end, { desc = "Find open buffer" })
+        map("n", "<Leader>ff", function() Snacks.picker.files() end, { desc = "Find files" })
+        map("n", "<Leader>fr", function() Snacks.picker.recent() end, { desc = "Find recent file" })
+        map("n", "<Leader>gd", function() Snacks.picker.git_diff() end, { desc = "Git diff" })
+        map("n", "<Leader>gl", function() Snacks.picker.git_log() end, { desc = "Git log" })
+        map("n", "<Leader>gL", function() Snacks.picker.git_log_line() end, { desc = "Git log line" })
+        map("n", "<Leader>gs", function() Snacks.picker.git_status() end, { desc = "Git status" })
+        map("n", "<Leader>gS", function() Snacks.picker.git_stash() end, { desc = "Git stash" })
+        map("n", "<Leader>ld", function() Snacks.picker.lsp_definitions() end, { desc = "Goto definition" })
+        map("n", "<Leader>lD", function() Snacks.picker.lsp_declarations() end, { desc = "Goto declaration" })
+        map("n", "<Leader>lI", function() Snacks.picker.lsp_implementations() end, { desc = "Goto implementation" })
+        map("n", "<Leader>lr", function() Snacks.picker.lsp_references() end, { nowait = true, desc = "Goto references" })
+        map("n", "<Leader>ly", function() Snacks.picker.lsp_type_definitions() end, { desc = "Goto type definition" })
+        map("n", "<Leader>rf", function() Snacks.rename.rename_file() end, { desc = "Rename file" })
+        map("n", "<Leader>sa", function() Snacks.picker.autocmds() end, { desc = "Search autocmds" })
+        map("n", "<Leader>sb", function() Snacks.picker.lines() end, { desc = "Grep buffer lines" })
+        map("n", "<Leader>sB", function() Snacks.picker.grep_buffers() end, { desc = "Grep open buffers" })
+        map("n", "<Leader>sd", function() Snacks.picker.diagnostics() end, { desc = "Search diagnostics" })
+        map("n", "<Leader>sD", function() Snacks.picker.diagnostics_buffer() end, { desc = "Search buffer diagnostics" })
+        map("n", "<Leader>sg", function() Snacks.picker.grep() end, { desc = "Grep" })
+        map("n", "<Leader>sh", function() Snacks.picker.help() end, { desc = "Search help" })
+        map("n", "<Leader>sH", function() Snacks.picker.highlights() end, { desc = "Search highlights" })
+        map("n", "<Leader>si", function() Snacks.picker.icons() end, { desc = "Search icons" })
+        map("n", "<Leader>sj", function() Snacks.picker.jumps() end, { desc = "Search jumps" })
+        map("n", "<Leader>sk", function() Snacks.picker.keymaps() end, { desc = "Search keymaps" })
+        map("n", "<Leader>sm", function() Snacks.picker.marks() end, { desc = "Search marks" })
+        map("n", "<Leader>sM", function() Snacks.picker.man() end, { desc = "Search man pages" })
+        map("n", "<Leader>sp", function() Snacks.picker.lazy() end, { desc = "Grep plugin spec" })
+        map("n", "<Leader>sq", function() Snacks.picker.qflist() end, { desc = "Search quickfix list" })
+        map("n", "<Leader>sr", function() Snacks.picker.resume() end, { desc = "Search resume" })
+        map("n", "<Leader>su", function() Snacks.picker.undo() end, { desc = "Search undo history" })
+        map({ "n", "x" }, "<Leader>sw", function() Snacks.picker.grep_word() end, { desc = "Grep visual selection or word" })
+        -- stylua: ignore end
+
         Snacks.toggle.option("conceallevel", { off = 0, on = 2, name = "Conceal Level" }):map("<Leader>tc")
         Snacks.toggle.diagnostics():map("<Leader>td")
         Snacks.toggle.dim():map("<Leader>tD")
-        -- TODO: Add buffer specific toggle
         Snacks.toggle({
             name = "Format on Save",
             get = function()
