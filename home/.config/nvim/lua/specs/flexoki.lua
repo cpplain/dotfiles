36a5f3b814ce79d467c6da880d6ec8c01af048b1
Christopher Plain <me@christopherplain.com>
2025-08-21 14:51:11 -0700

    Move flexoki opts into config

--- home/.config/nvim/lua/specs/flexoki.lua
+++ home/.config/nvim/lua/specs/flexoki.lua
@@ -1,18 +1,16 @@
 return {
     "cpplain/flexoki.nvim",
     -- dir = "~/git/flexoki.nvim",
-    lazy = false,
     priority = 1000,
-
-    ---@type flexoki.Config
-    opts = {
-        plugins = {
-            "gitsigns",
-            "nvim_treesitter_context",
-            "which_key",
-        },
-    },
-    config = function(_, opts)
+    config = function()
+        ---@type flexoki.Config
+        local opts = {
+            plugins = {
+                "gitsigns",
+                "nvim_treesitter_context",
+                "which_key",
+            },
+        }
         require("flexoki").setup(opts)
         vim.cmd.colorscheme("flexoki")
     end,
