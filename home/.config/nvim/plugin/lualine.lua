226bf11e68172bf3f54c671bb417504977adcae9
Christopher Plain <me@christopherplain.com>
2025-08-28 15:15:12 -0700

    feat(neovim): change lualine mode to first letter

--- home/.config/nvim/plugin/lualine.lua
+++ home/.config/nvim/plugin/lualine.lua
@@ -6,5 +6,11 @@ require("lualine").setup({
         component_separators = "",
         disabled_filetypes = { statusline = { "snacks_dashboard" } },
     },
+    sections = {
+        lualine_a = {
+            --stylua: ignore
+            { "mode", fmt = function(str) return str:sub(1, 1) end, },
+        },
+    },
     extensions = { "lazy", "oil", "quickfix" },
 })
