18518fb921d2b36b6d8a67101b2c94e918b86d02
Christopher Plain <me@christopherplain.com>
2025-09-08 21:01:50 -0700

    fix(script): correct ansi escape codes

--- scripts/display_colors.sh
+++ scripts/display_colors.sh
@@ -1,11 +1,11 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 echo -e "\033[0mNO_COLOR"
-echo -e "\033[0;30mBLACK\t\t\033[1;30mDARK_GRAY"
-echo -e "\033[0;31mRED\t\t\033[1;31mLIGHT_RED"
-echo -e "\033[0;32mGREEN\t\t\033[1;32mLIGHT_GREEN"
-echo -e "\033[0;33mYELLOW\t\t\033[1;33mLIGHT_YELLOW"
-echo -e "\033[0;34mBLUE\t\t\033[1;34mLIGHT_BLUE"
-echo -e "\033[0;35mPURPLE\t\t\033[1;35mLIGHT_PURPLE"
-echo -e "\033[0;36mCYAN\t\t\033[1;36mLIGHT_CYAN"
-echo -e "\033[0;37mLIGHT_GRAY\t\033[1;37mWHITE"
+echo -e "\033[30mBLACK\t\t\033[90mDARK_GRAY"
+echo -e "\033[31mRED\t\t\033[91mLIGHT_RED"
+echo -e "\033[32mGREEN\t\t\033[92mLIGHT_GREEN"
+echo -e "\033[33mYELLOW\t\t\033[93mLIGHT_YELLOW"
+echo -e "\033[34mBLUE\t\t\033[94mLIGHT_BLUE"
+echo -e "\033[35mPURPLE\t\t\033[95mLIGHT_PURPLE"
+echo -e "\033[36mCYAN\t\t\033[96mLIGHT_CYAN"
+echo -e "\033[37mLIGHT_GRAY\t\033[97mWHITE"
